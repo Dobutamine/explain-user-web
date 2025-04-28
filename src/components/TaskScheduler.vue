28314cece87288be9f1884df006af5eb1e0764b0
dobutamine <timantonius@pm.me>
2025-04-28 20:01:02 +0200

    task scheduler

--- src/components/TaskScheduler.vue
+++ src/components/TaskScheduler.vue
@@ -4,6 +4,43 @@
         {{ title }}
       </div>
       <div v-if="isEnabled">
+        <q-card class="q-pb-xs q-pt-xs q-ma-sm" bordered>
+            <div class="q-ma-sm row">
+              <div class="col">property</div>
+              <div class="col">current value</div>
+              <div class="col">target value</div>
+              <div class="col">change in (s)</div>
+              <div class="col-1"></div>
+              <div class="col-1"></div>
+            </div>
+            <q-list bordered separator dense>
+            <div v-for="(task, index) in task_list" :key="index">
+              <q-item clickable v-ripple dark dense @click="selectTask">
+                <q-item-section>
+                  <div class="q-ma-xs row">
+                    <div class="col">{{ task.prop }}</div>
+                    <div class="col">{{ task.from }}</div>
+                    <div class="col">{{ task.target }}</div>
+                    <div class="col">{{ task.in }}</div>
+                    <div class="col-1">
+                      <q-btn color="black" size="xs" dense
+                      icon="fa-solid fa-chevron-up" style="font-size: 8px" @click="moveUp"><q-tooltip>run</q-tooltip></q-btn>
+                      <q-btn class="q-ml-sm" color="black" size="xs" dense
+                      icon="fa-solid fa-chevron-down" style="font-size: 8px" @click="moveDown"><q-tooltip>delete</q-tooltip></q-btn>
+                    </div>
+                    <div class="col-1">
+                      <q-btn color="primary" size="xs" dense
+                      icon="fa-solid fa-play" style="font-size: 8px"><q-tooltip>run</q-tooltip></q-btn>
+                      <q-btn class="q-ml-sm" color="negative" size="xs" dense
+                      icon="fa-solid fa-trash" style="font-size: 8px"><q-tooltip>delete</q-tooltip></q-btn>
+                    </div>
+
+                  </div>
+                </q-item-section>
+              </q-item>
+            </div>
+          </q-list>
+        </q-card>
       </div>
     </q-card>
   </template>
@@ -14,12 +51,25 @@
     data() {
       return {
         isEnabled: true,
-        title: "TASK SCHEDULER"
+        title: "TASK SCHEDULER",
+        task_list: [
+          { prop: "AA.el_base", from: 50, target: 100, in: 5, at: 0},
+          { prop: "LV.el_min", from: 30, target: 200, in: 5, at: 0},
+        ]
       };
     },
     methods: {
       toggle() {
         this.isEnabled = !this.isEnabled
+      },
+      moveUp() {
+        console.log('move up')
+      },
+      moveDown() {
+        console.log('move up')
+      },
+      selectTask(e) {
+        console.log(e)
       }
     },
     beforeUnmount() {
