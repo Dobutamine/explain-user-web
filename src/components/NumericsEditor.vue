aa3c666e20b980c3b39a0deed58c3b88615777be
dobutamine <timantonius@pm.me>
2025-07-20 17:51:43 +0200

    progress

--- src/components/NumericsEditor.vue
+++ src/components/NumericsEditor.vue
@@ -16,7 +16,184 @@
                     icon="fa-solid fa-plus" @click="buildMonitor" style="font-size: 8px"><q-tooltip>add new monitor</q-tooltip></q-btn>
                 <q-btn v-if="selectedMonitorName" class="col-1 q-ma-xs q-mt-md" color="grey-9" size="xs" dense
                     icon="fa-solid fa-xmark" @click="cancelMonitor" style="font-size: 8px"><q-tooltip>clear new monitor</q-tooltip></q-btn>
-          </div>
+            </div>
+
+            <div v-if="selectedMonitor">
+                <div class="q-ml-md q-mr-md q-mt-md text-left text-secondary" :style="{ 'font-size': '12px' }">
+                    <div class="text-white" :style="{ 'font-size': '10px' }">
+                        <q-input 
+                            v-model="selectedMonitor.title" 
+                            label="title" 
+                            color="blue" 
+                            hide-hint 
+                            filled 
+                            dense 
+                            stack-label 
+                            style="font-size: 12px" 
+                            class="q-mb-sm" 
+                            squared>
+                        </q-input>
+                    </div>
+                </div>
+                
+                <div class="q-ml-lg q-mr-md q-mt-md text-left text-secondary row" :style="{ 'font-size': '12px' }">
+                    <div class="col"> enabled </div>
+                    <div class="col-2 text-white" :style="{ 'font-size': '10px' }">
+                      <q-toggle 
+                        v-model="selectedMonitor.enabled" 
+                        color="primary" 
+                        size="sm" 
+                        hide-hint 
+                        filled 
+                        dense 
+                        style="font-size: 12px" 
+                        class="q-mb-sm">
+                      </q-toggle>
+                    </div>
+                </div>
+
+                <div class="q-ml-lg q-mr-md q-mt-md text-left text-secondary row" :style="{ 'font-size': '12px' }">
+                    <div class="col"> collapsed </div>
+                    <div class="col-2 text-white" :style="{ 'font-size': '10px' }">
+                      <q-toggle 
+                        v-model="selectedMonitor.collapsed" 
+                        color="primary" 
+                        size="sm" 
+                        hide-hint 
+                        filled 
+                        dense 
+                        style="font-size: 12px" 
+                        class="q-mb-sm">
+                      </q-toggle>
+                    </div>
+                </div>
+
+                <div class="q-ma-md text-left text-secondary" :style="{ 'font-size': '12px' }">
+                <div v-for="(parameter, index) in selectedMonitor.parameters" :key="index">
+                    <div class="q-ma-sm text-center text-white">PARAMETER {{ index + 1 }}</div>
+                        <q-separator></q-separator>
+                        <div v-if="parameter.props_processed.length > 0" class="q-ma-sm">SETTINGS</div>
+                        <div class="text-white" :style="{ 'font-size': '10px' }">
+                            <q-input 
+                                v-model="parameter.label" 
+                                label="label" 
+                                color="blue" 
+                                hide-hint 
+                                filled 
+                                dense 
+                                stack-label 
+                                style="font-size: 12px" 
+                                class="q-mb-sm" 
+                                squared>
+                            </q-input>
+                        </div>
+                        <div class="text-white" :style="{ 'font-size': '10px' }">
+                            <q-input 
+                                v-model="parameter.unit" 
+                                label="unit" 
+                                color="blue" 
+                                hide-hint 
+                                filled 
+                                dense 
+                                stack-label 
+                                style="font-size: 12px" 
+                                class="q-mb-sm" 
+                                squared>
+                            </q-input>
+                        </div>
+                        <div class="text-white" :style="{ 'font-size': '10px' }">
+                            <q-input 
+                                v-model.number="parameter.factor" 
+                                label="factor" 
+                                color="blue" 
+                                hide-hint 
+                                filled 
+                                dense 
+                                stack-label 
+                                style="font-size: 12px" 
+                                class="q-mb-sm" 
+                                squared>
+                            </q-input>
+                        </div>
+                         <div class="text-white" :style="{ 'font-size': '10px' }">
+                            <q-input 
+                                v-model.number="parameter.rounding" 
+                                label="rounding" 
+                                color="blue" 
+                                hide-hint 
+                                filled 
+                                dense 
+                                stack-label 
+                                style="font-size: 12px" 
+                                class="q-mb-sm" 
+                                squared>
+                            </q-input>
+                        </div>
+                        <div class="q-ma-sm text-left text-white row" :style="{ 'font-size': '12px' }">
+                            <div class="col"> weight based </div>
+                            <div class="col-2 text-white" :style="{ 'font-size': '10px' }">
+                                <q-toggle 
+                                        v-model="parameter.weight_based" 
+                                        color="primary" 
+                                        size="sm" 
+                                        hide-hint 
+                                        filled 
+                                        dense 
+                                        style="font-size: 12px" 
+                                        class="q-mb-sm">
+                                </q-toggle>
+                            </div>
+                        </div> 
+                        <div v-if="parameter.props_processed.length > 0" class="q-ma-sm">PROPERTIES</div>
+                        <div v-for="(prop, index_prop) in parameter.props_processed" :key="index_prop">
+                            <div class="q-mt-sm text-white" :style="{ 'font-size': '10px' }">
+                                <q-select 
+                                    v-model="prop.model" 
+                                    label="model" 
+                                    :options="availableModels" 
+                                    color="blue"
+                                    hide-hint 
+                                    filled 
+                                    dense  
+                                    stack-label
+                                    style="font-size: 12px" 
+                                    class="q-mb-sm"
+                                    @update:model-value="selectPropModel(prop, prop.model)"
+                                    squared>
+                                </q-select>
+
+                            </div>
+                            <div class="text-white" :style="{ 'font-size': '10px' }">
+                                <q-select 
+                                    v-model="prop.prop" 
+                                    label="model" 
+                                    :options="prop.avalaibleProperties" 
+                                    color="blue"
+                                    hide-hint 
+                                    filled 
+                                    dense  
+                                    stack-label
+                                    style="font-size: 12px" 
+                                    class="q-mb-sm"
+                                    squared>
+                                </q-select>
+                            </div>
+                            <div class="row q-ma-md">
+                            <q-btn class="col q-ml-xl q-mr-xl" color="negative" size="sm" dense @click="removeProperty(parameter, index_prop)"
+                                style="font-size: 10px"><q-icon class="fa-solid fa-trash-can"></q-icon><q-tooltip>remove model property</q-tooltip></q-btn>
+                            </div>
+                        </div>
+                        <div class="row q-ma-md">
+                            <q-btn class="col q-ml-xl q-mr-xl" color="primary" size="sm" dense @click="addProperty(parameter)"
+                            style="font-size: 10px">ADD PROPERTY<q-tooltip>add a model property</q-tooltip></q-btn>
+                        </div>
+
+                </div>
+
+                <q-separator bg-separator></q-separator>
+                </div>
+            </div>
+
         </div>
       </q-card>
     </div>
@@ -25,6 +202,7 @@
 
 <script>
 
+import { checkMaxIfStatementsInShader } from "pixi.js";
 import { explain } from "../boot/explain";
 import { useStateStore } from "src/stores/state";
 
@@ -46,16 +224,85 @@ export default {
         availableMonitors: [],
         selectedMonitorName: "",
         availableMonitorNames: [],
+        availableModels: [],
+        avalaibleProperties: [],
     };
   },
   methods: {
-    buildMonitor() {},
+    selectPropModel(prop_current, prop_model) {
+        prop_current['avalaibleProperties'] = []
+        prop_current.prop = ""
+        Object.keys(explain.modelState.models[prop_model]).forEach(prop => {
+          if (typeof (explain.modelState.models[prop_model][prop]) === 'number') {
+            if (prop[0] !== "_") {
+              prop_current['avalaibleProperties'].push(prop)
+            }
+          }
+        })
+        prop_current['avalaibleProperties'].sort()
+        console.log(prop_current['avalaibleProperties'])
+    },
+    addProperty(parameter) {
+        let new_prop = {
+            model: "",
+            prop: ""
+        }
+        parameter.props_processed.push(new_prop)
+    },
+    removeProperty(parameter, prop_index) {
+        console.log(parameter)
+        parameter.props_processed.splice(prop_index, 1);
+    },
+    buildMonitor() {
+        this.selectedMonitor = {
+            title: "",
+            enabled: false,
+            collapsed: false,
+            parameters: [
+                {
+                    label: "new_param",
+                    unit: "",
+                    factor: 1.0,
+                    rounding: 0,
+                    weight_based: false,
+                    props: []
+                }
+            ]
+        }
+    },
     cancelMonitor() {
         this.selectedMonitorName = ""
+        this.selectedMonitor = ""
     },
-    monitorSelected() {}
+    monitorSelected() {
+        this.selectedMonitor = {...this.state.configuration.monitors[this.selectedMonitorName]}
+        this.selectedMonitor.parameters.forEach(parameter => {
+            parameter.props_processed = []
+            parameter.props.forEach(prop => {
+                let p = prop.split(".")
+                parameter.props_processed.push({ model: p[0], prop: p[1]})
+            })
+        })
+    },
+    processAvailableModels() {
+        this.availableModels = []
+        try {
+            if (Object.keys(explain.modelState.models)) {
+            this.availableModels = [...Object.keys(explain.modelState.models)].sort();
+            }
+        } catch { }
+    },
+    processModelInterface() {
+        Object.values(explain.modelState.models[this.selectedModel]).forEach(model => {
+            if (prop.options.includes(model.model_type)) {
+                prop["choices"].push(model.name)
+              }
+        })
+    }
   },
   beforeUnmount() {
+    this.$bus.off("state", this.processAvailableModels)
+    this.$bus.off("model_interface",  this.processModelInterface)
   },
   mounted() {
     // process the available monitors
@@ -63,6 +310,8 @@ export default {
     Object.keys(this.state.configuration.monitors).forEach(monitor_name => {
         this.availableMonitorNames.push(monitor_name)
     })
+    this.$bus.on("state", this.processAvailableModels)
+    this.$bus.on("model_interface",  this.processModelInterface)
   },
 };
 </script>
