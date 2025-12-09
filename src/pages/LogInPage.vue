866d2f285223e35933d78b175ec8f2ab24fc35df
dobutamine <timantonius@pm.me>
2025-12-09 09:06:20 +0100

    enable auto login

--- src/pages/LogInPage.vue
+++ src/pages/LogInPage.vue
@@ -286,12 +286,12 @@ export default {
       this.newUserEntry = false;
     });
 
-    // if (process.env.DEV) {
-    //   //override login for developement development
-    //   this.password = "y5qkqjed";
-    //   this.name = "timothy_exp";
-    //   this.user.logIn(this.general.apiUrl, this.name, this.password);
-    // }
+    if (process.env.DEV) {
+      //override login for developement development
+      this.password = "y5qkqjed";
+      this.name = "timothy_exp";
+      this.user.logIn(this.general.apiUrl, this.name, this.password);
+    }
   },
 };
 </script>
